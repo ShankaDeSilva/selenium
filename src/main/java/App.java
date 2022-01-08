3fce8cab62f67ea478e7c96d77d38d071f5449ec
ShankaDeSilva <shankadesilva.c@gmail.com>
2022-01-08 11:32:47 +0530

    Types of Waits

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,21 +1,44 @@
+import com.google.common.base.Function;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.sql.DriverManager;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        //Implicit Wait
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize(); //Open chrome browser as maximize
-        driver.get("https://en.wikipedia.org/wiki/Sri_Lanka");
+        driver.get("https://www.wikipedia.org/");
 
+        System.out.println(driver.getTitle());
 
+        //Explicit Wait
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("js-link-box-si"))));
+
+        //Fluent Wait
+        Wait fluentWait = new FluentWait<WebDriver>(driver)
+                .withTimeout(15,TimeUnit.SECONDS)
+                .pollingEvery(5,TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        fluentWait.until(new Function<WebDriver, WebElement>(){
+           public WebElement apply(WebDriver driver){
+               return driver.findElement(By.id("js-link-box-si"));
+           }
+        });
 
 //        //Working with Tables-------------------------------------------------------------------------------
 //        System.out.println(driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div[1]/table[4]/tbody/tr/td/table/tbody/tr[1]/td[1]")).getText());
